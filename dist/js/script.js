f458e7e0c27777b10153046dd1bffbf04f0722d1
jufentosemrylake <jufentosemry@gmail.com>
2022-04-14 19:06:14 +0700

    edit dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,10 +2,15 @@
 window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -23,3 +28,36 @@ hamburger.addEventListener('click', function () {
 var tahun = new Date();
 // const tahun = document.querySelector('.tahun');
 document.getElementById("tahun").innerHTML = tahun.getFullYear();
+
+//click dimana saja
+window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+        
+    }
+})
+
+//darkmode toggle
+const darktoggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+darktoggle.addEventListener('click', function () {
+    if (darktoggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+//pindahkan posisi toggle sesuai mode
+if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    darktoggle.checked = true;
+} else {
+      darktoggle.checked = false;
+  }
